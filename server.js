3c2fc10b905eec9a268edd7dab3df42bf2321ffb
himanshu-intruder <73782865+himanshu-intruder@users.noreply.github.com>
2026-02-28 03:02:47 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// This tells the server to show your index.html file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
